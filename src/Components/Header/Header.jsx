5685cd02c3f9971847c9615b49ab7685563c2682
Midnight <MidnightSP@yahoo.com>
2023-05-25 00:12:17 -0700

    - Added hamburger

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.scss'
 
 export const Header = () => {
 
+    const [ showBurgerMenu, setShowBurgerMenu ] = useState(false);
+
     const scrollTo =(selectedSection)=>{
+        setShowBurgerMenu(false);
         const scrollSection = document.getElementById(`${selectedSection}`);
         scrollSection.scrollIntoView({behavior: "smooth", block: 'start'});
     }
 
     return (
         <div className="header-wrapper">
-            <h5 onClick={()=>scrollTo("#projects")}>Code Projects</h5>
-            <h5 onClick={()=>scrollTo("#technologies")}>Technologies</h5>
-            <h5 onClick={()=>scrollTo("#otherTech")}>Other Tech</h5>
-            <h5 onClick={()=>scrollTo("#art")}>Art</h5>
-            {/* 
-            <h5 onClick={()=>scrollTo("#gaming")}>Gaming</h5>
-            <h5 onClick={()=>scrollTo("#blog")}>Blog</h5>
-            <h5 onClick={()=>scrollTo("#about")}>About</h5> */}
+            <div className="main-menu">
+                <h5 onClick={()=>scrollTo("#projects")}>Code Projects</h5>
+                <h5 onClick={()=>scrollTo("#technologies")}>Technologies</h5>
+                <h5 onClick={()=>scrollTo("#otherTech")}>Other Tech</h5>
+                <h5 onClick={()=>scrollTo("#art")}>Art</h5>
+                {/* 
+                <h5 onClick={()=>scrollTo("#gaming")}>Gaming</h5>
+                <h5 onClick={()=>scrollTo("#blog")}>Blog</h5>
+                <h5 onClick={()=>scrollTo("#about")}>About</h5> */}
+            </div>
+            <div className="burger-menu">
+                <svg onClick={()=>setShowBurgerMenu(!showBurgerMenu)}fill="#ffd06c" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4 6H20M4 12H20M4 18H20" stroke="#ffd06c" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                </svg>
+                <div className={`fries ${showBurgerMenu ? "" : "display-none"} `}>
+                    <h5 onClick={()=>scrollTo("#projects")}>Code Projects</h5>
+                    <h5 onClick={()=>scrollTo("#technologies")}>Technologies</h5>
+                    <h5 onClick={()=>scrollTo("#otherTech")}>Other Tech</h5>
+                    <h5 onClick={()=>scrollTo("#art")}>Art</h5>
+                    {/* 
+                    <h5 onClick={()=>scrollTo("#gaming")}>Gaming</h5>
+                    <h5 onClick={()=>scrollTo("#blog")}>Blog</h5>
+                    <h5 onClick={()=>scrollTo("#about")}>About</h5> */}
+                </div>
+            </div>
         </div>
     )
 }
